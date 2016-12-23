80c93f3153f80792a1fc77d4b4b640be042fec0c
Tony Atkins <tony@raisingthefloor.org>
2016-12-23 12:24:02 +0100

    UL-110:  Initial implementation of metadata “read” endpoints.

--- src/js/server/metadata/metadata-read.js
+++ src/js/server/metadata/metadata-read.js
@@ -2,6 +2,28 @@
 
     The "read" portion of the GET /api/images/metadata endpoint.
 
+    The views that back this endpoint use compound keys as outlined here:
+
+    http://ryankirkman.com/2011/03/30/advanced-filtering-with-couchdb-views.html
+
+    So, for `GET /api/images/metadata/:uid`, the request is something like:
+
+    http://localhost:7318/images/_design/metadata/_view/combined?startkey=[%221421059432806-826608318%22]&endkey=[%221421059432806-826608318%22,{},{}]
+
+    Note that the end of the range has two empty objects, which means we'll get every record with the first part of the compound key.
+
+    For `GET /api/images/metadata/:uid/:source`, the request is something like:
+
+    http://localhost:7318/images/_design/metadata/_view/combined?startkey=[%221421059432806-826608318%22,%20%22unified%22]&endkey=[%221421059432806-826608318%22,%20%22unified%22,%20{}]
+
+    There we supply the first two pieces, and omit the third.
+
+    For `GET /api/images/metadata/:uid/:source/:image_id`, the request is something like:
+
+    http://localhost:7318/images/_design/metadata/_view/combined?key=[%221421059432806-826608318%22,%20%22contributor%22,%20%22e.svg%22]
+
+    There we supply all three pieces (and use the `key` instead of `startkey` and `endkey`.
+
  */
 "use strict";
 var fluid = require("infusion");
@@ -12,39 +34,91 @@ var gpii   = fluid.registerNamespace("gpii");
 fluid.require("%gpii-express");
 fluid.require("%gpii-json-schema");
 
+require("../source-permission-middleware");
+require("../view-read-dataSource");
+
+fluid.registerNamespace("gpii.ul.api.images.metadata.read.dataSource");
+
+gpii.ul.api.images.metadata.read.dataSource.filterBySource = function (handler, unfilteredResults) {
+    var user = handler.options.request.session && handler.options.request.session[handler.options.sessionKey];
+    var authorizedSources = gpii.ul.api.sources.request.listSources(gpii.ul.api.sources.sources, user, "view");
+
+    var filteredResults = fluid.copy(unfilteredResults);
+    fluid.remove_if(filteredResults, function (record) {
+        // Remove the record from the list if its source is not one we're authorized to see.
+        return Boolean(authorizedSources.indexOf(record.source) === -1);
+    });
+
+    return filteredResults;
+};
+
+// Our "base" dataSource, designed to return a single record from an array of view results.
+fluid.defaults("gpii.ul.api.images.metadata.read.dataSource", {
+    gradeNames: ["gpii.ul.images.dataSources.couch"],
+    baseUrl: "{gpii.ul.api}.options.urls.imageDb",
+    endpoint: "/_design/metadata/_view/combined",
+    listeners: {
+        // Strip out any records we are not allowed to see
+        "onRead.filterBySource": {
+            namespace: "filter",
+            priority:  "after:transform",
+            funcName:  "gpii.ul.api.images.metadata.read.dataSource.filterBySource",
+            args:      ["{gpii.express.handler}", "{arguments}.0"]
+        },
+        // On a successful read, send the transformed results.
+        "onRead.sendResponse": {
+            priority: "after:filter",
+            func:     "{gpii.express.handler}.sendResponse",
+            args:     [ 200, "{arguments}.0.0"] // statusCode, body
+        },
+        "onError.handleError": {
+            func: "gpii.ul.api.images.metadata.read.handler.handleError",
+            args: [ "{that}", "{arguments}.0."] // errorResponse
+        }
+    }
+});
+
 fluid.registerNamespace("gpii.ul.api.images.metadata.read.handler");
 
-require("../source-permission-middleware");
-
-/*
-
- */
-
-
-
- // TODO:  All must strip "type", "_id", and "_rev" from the raw couch records
-
 gpii.ul.api.images.metadata.read.handler.handleRequest = function (that) {
-/*
- // See: http://ryankirkman.com/2011/03/30/advanced-filtering-with-couchdb-views.html
+    var userOptions = fluid.model.transformWithRules(that.options.request, that.options.rules.requestContentToValidate);
 
- # `GET /api/images/metadata/:uid`
- // /:uid
- // http://localhost:7318/images/_design/metadata/_view/combined?startkey=[%221421059432806-826608318%22]&endkey=[%221421059432806-826608318%22,{},{}]
- # `GET /api/images/metadata/:uid/:source`
- // /:uid/:source
- // http://localhost:7318/images/_design/metadata/_view/combined?startkey=[%221421059432806-826608318%22,%20%22unified%22]&endkey=[%221421059432806-826608318%22,%20%22unified%22,%20{}]
- # `GET /api/images/metadata/:uid/:source/:image_id`
- // /:uid/:source/:image_id
- // http://localhost:7318/images/_design/metadata/_view/combined?key=[%221421059432806-826608318%22,%20%22contributor%22,%20%22e.svg%22]
- */
+    var requestData = {
+        startkey: [userOptions.uid],
+        endkey:   [userOptions.uid, {}, {}]
+    };
 
-    that.sendResponse(404, { isError: true, message: that.options.messages.fileNotFound});
+    if (userOptions.source) {
+        requestData.startkey.push(userOptions.source);
+        requestData.endkey[1] = userOptions.source;
+    }
+
+    // If we have all three options, request a single record.
+    if (userOptions.image_id) {
+        requestData.startkey.push(userOptions.image_id);
+        that.singleRecordReader.get({ key: requestData.startkey });
+    }
+    // Otherwise, request multiple records.
+    else {
+        that.multiRecordReader.get(requestData);
+    }
+
+};
+
+gpii.ul.api.images.metadata.read.handler.handleError = function (that, response) {
+    var statusCode = response.statusCode || 500;
+    if (statusCode === 404) {
+        that.sendResponse( statusCode, { isError: true, message: that.options.messages.notFound});
+    }
+    else {
+        that.sendResponse( statusCode, response);
+    }
 };
 
 fluid.defaults("gpii.ul.api.images.metadata.read.handler", {
     gradeNames: ["gpii.express.handler"],
     messages: {
+        notFound: "The image ID you have supplied does not correspond to an existing metadata record."
     },
     invokers: {
         handleRequest: {
@@ -53,30 +127,21 @@ fluid.defaults("gpii.ul.api.images.metadata.read.handler", {
         }
     },
     components: {
-        metadataReader: {
-            type: "kettle.dataSource.URL",
+        singleRecordReader: {
+            type: "gpii.ul.api.images.metadata.read.dataSource"
+        },
+        // A dataSource designed to filter a larger array of records by source.  Required because of the
+        // `GET /api/images/metadata/:uid` endpoint, which would otherwise return information about records
+        // the current user should not be able to see.
+        multiRecordReader: {
+            type: "gpii.ul.api.images.metadata.read.dataSource",
             options: {
-                url: {
-                    expander: {
-                        funcName: "fluid.stringTemplate",
-                        args: ["%baseUrl/_design/ul/_view/products?key=%key", { baseUrl: "{gpii.ul.api}.options.urls.ulDb" }]
-                    }
-                },
-                termMap: {
-                    "key":      "%key"
-                },
                 listeners: {
-                    // Continue processing after an initial successful read.
-                    "onRead.processProductResponse": {
-                        func: "{gpii.ul.api.product.get.handler.base}.processProductResponse",
-                        args: ["{arguments}.0"] // couchResponse
-                    },
-                    // Report back to the user on failure.
-                    "onError.sendResponse": {
+                    // Send the filtered results to the user.
+                    "onRead.sendResponse": {
+                        priority: "after:filter",
                         func: "{gpii.express.handler}.sendResponse",
-                        args: [ 500, { message: "{arguments}.0", url: "{that}.options.url" }] // statusCode, body
-                        // args: [ 500, "{arguments}.0"] // statusCode, body
-                        // TODO:  Discuss with Antranig how to retrieve HTTP status codes from kettle.datasource.URL
+                        args: [ 200, "{arguments}.0"] // statusCode, body
                     }
                 }
             }
